c40ae11476dc581f45b2f611fa034c020d76e24f
KaOz25 <karimozael@gmail.com>
2020-12-04 15:49:53 -0600

    primer

---
+++ Lab7_KarimGuifarro/src/lab7_karimguifarro/Lab7_KarimGuifarro.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab7_karimguifarro;
+
+/**
+ *
+ * @author Karim Ozael
+ */
+public class Lab7_KarimGuifarro {
+
+    
+    public static void main(String[] args) {
+        
+    }
+    
+}
